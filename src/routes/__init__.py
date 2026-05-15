a069476d9a60d28a4e93c9681552f98b8b9f3961
Daniel Mauricio Padilla González <167046547+mahurisio@users.noreply.github.com>
2026-05-15 14:53:05 -0500

    Corregir importaciones del paquete de rutas

--- src/routes/__init__.py
+++ src/routes/__init__.py
@@ -1,4 +1,4 @@
-from routes.assets import router as assets_router
-from routes.work_orders import router as work_orders_router
+from src.routes.assets import router as assets_router
+from src.routes.work_orders import router as work_orders_router
 
 __all__ = ["assets_router", "work_orders_router"]
