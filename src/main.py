ce2f908827754bac0ad27f9ac807e30e82135198
Daniel Mauricio Padilla González <167046547+mahurisio@users.noreply.github.com>
2026-05-15 14:30:40 -0500

    Corregir importacion del punto de entrada

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from app import create_app
+from src.app import create_app
 
 app = create_app()
 
@@ -6,4 +6,4 @@ app = create_app()
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run("src.main:app", host="0.0.0.0", port=8000, reload=True)
